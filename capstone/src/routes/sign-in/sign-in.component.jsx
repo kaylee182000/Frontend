e7e9f5c8c59281b26cca9e18840d781a0ab48e62
kaylee182000 <trungkien40414@gmail.com>
2023-05-16 23:39:49 +0700

    confirguration for google sign in redirect

--- capstone/src/routes/sign-in/sign-in.component.jsx
+++ capstone/src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+  auth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  //firebase google redirect ways
+  // useEffect(() => {
+  //   getRedirectResponse();
+  // }, []);
+
+  // const getRedirectResponse = async () => {
+  //   const res = await getRedirectResult(auth);
+  //   if (res) {
+  //     const userDocRef = await createUserDocumentFromAuth(res.user);
+  //   }
+  // };
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In</h1>
       <button onClick={logGoogleUser}>Sign in with Google</button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
     </div>
   );
 };
