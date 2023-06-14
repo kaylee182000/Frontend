2215916947e11aa99f9c2e7a5c3b97bccc21be9d
NBVO4F-IT-DEV01\nobland <trungkien@nobland.com>
2023-06-14 15:28:26 +0700

    Done Page hero, navbar and footer

---
+++ capstone3_nextjs/components/index.ts
@@ -0,0 +1,6 @@
+import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+export { Hero, CustomButton, NavBar, Footer };
