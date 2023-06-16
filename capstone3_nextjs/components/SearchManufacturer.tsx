d56eb26ed0a1fdc68d5628e52b4e714d8719d33a
NBVO4F-IT-DEV01\nobland <trungkien@nobland.com>
2023-06-16 23:20:50 +0700

    Done with search by manufacturer

--- capstone3_nextjs/components/SearchManufacturer.tsx
+++ capstone3_nextjs/components/SearchManufacturer.tsx
@@ -1,19 +1,32 @@
 "use client";
 
-import React from "react";
-
+import React, { useState, Fragment } from "react";
 import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
 
 import { SearchManufacturesProps } from "@/types";
 
+import { manufacturers } from "@/constants";
+
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturesProps) => {
+  const [query, setQuery] = useState("");
+
+  const filterManufacturer =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -27,8 +40,53 @@ const SearchManufacturer = ({
           <Combobox.Input
             className="search-manufacturer__input"
             placeholder="Volkswagen"
-            displayValue={(manufacturer) => manufacturer}
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
           />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options>
+              {filterManufacturer.length === 0 && query !== "" ? (
+                <div className="search-manufacturer__option">Nothing found</div>
+              ) : (
+                filterManufacturer.map((item) => (
+                  <Combobox.Option
+                    value={item}
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                      } `
+                    }
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-white" : "text-teal-600"
+                            }`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
         </div>
       </Combobox>
     </div>
