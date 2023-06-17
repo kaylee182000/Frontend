ad12c0dd5ea4745c47ab98ba9f603d76e26b75f9
NBVO4F-IT-DEV01\nobland <trungkien@nobland.com>
2023-06-17 17:31:18 +0700

    Done rendering a car using rapid api and iamgin.carstudio

--- capstone3_nextjs/types/index.ts
+++ capstone3_nextjs/types/index.ts
@@ -3,8 +3,11 @@ import { ButtonHTMLAttributes, MouseEventHandler } from "react";
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
+  textStyle?: string;
+  rightIcon?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  isDisabled?: boolean;
 }
 
 export interface CustomFilterProps {
@@ -14,4 +17,25 @@ export interface CustomFilterProps {
 export interface SearchManufacturesProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+  class: string;
+}
+
+export interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
 }
