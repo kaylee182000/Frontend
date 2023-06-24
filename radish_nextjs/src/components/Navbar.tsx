3c72004742539d0a0cd2beb49caf06892d4a46cc
kaylee182000 <trungkien40414@gmail.com>
2023-06-24 23:17:55 +0700

    Done w login

--- radish_nextjs/src/components/Navbar.tsx
+++ radish_nextjs/src/components/Navbar.tsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 import { Icon } from "./";
 import { buttonVariants } from "./ui/Button";
 import { cn } from "@/lib/utils";
+import { getAuthSession } from "@/lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getAuthSession();
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -20,12 +22,14 @@ const Navbar = () => {
         {/* {search bar} */}
 
         {/* {signin} */}
-        <Link
-          href={"/sign-in"}
-          className={cn(buttonVariants(), "hover:bg-red-rad")}
-        >
-          Sign In
-        </Link>
+        {!session && (
+          <Link
+            href={"/sign-in"}
+            className={cn(buttonVariants(), "hover:bg-red-rad")}
+          >
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
