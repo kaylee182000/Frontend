23498efd57636ef484362f25d7df08178d40fdec
kaylee182000 <trungkien40414@gmail.com>
2023-06-29 23:14:17 +0700

    feat: Done setup mutation for upvote and down vote button

--- radish_nextjs/src/components/post-vote/PostVoteClient.tsx
+++ radish_nextjs/src/components/post-vote/PostVoteClient.tsx
@@ -7,6 +7,10 @@ import React, { useEffect, useState } from "react";
 import { Button } from "../ui/Button";
 import { ArrowBigDown, ArrowBigUp } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { toast } from "@/hooks/use-toast";
+import { PostVoteRequest } from "@/lib/validators/vote";
 
 interface PostVoteClientProps {
   postId: string;
@@ -21,7 +25,7 @@ const PostVoteClient = ({
 }: PostVoteClientProps) => {
   const { loginToast } = useCustomToast();
 
-  const [voteAmt, setVoteAmt] = useState<number>(initialVotesAmt);
+  const [votesAmt, setVotesAmt] = useState<number>(initialVotesAmt);
 
   const [currentVote, setCurrentVote] = useState(initialVote);
 
@@ -31,9 +35,58 @@ const PostVoteClient = ({
     setCurrentVote(initialVote);
   }, [initialVote]);
 
+  const { mutate: vote } = useMutation({
+    mutationFn: async (type: VoteType) => {
+      const payload: PostVoteRequest = {
+        voteType: type,
+        postId: postId,
+      };
+
+      await axios.patch("/api/subreddit/post/vote", payload);
+    },
+    onError: (err, voteType) => {
+      if (voteType === "UP") setVotesAmt((prev) => prev - 1);
+      else setVotesAmt((prev) => prev + 1);
+
+      // reset current vote
+      setCurrentVote(prevVote);
+
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: "Something went wrong.",
+        description: "Your vote was not registered. Please try again.",
+        variant: "destructive",
+      });
+    },
+    onMutate: (type: VoteType) => {
+      if (currentVote === type) {
+        // User is voting the same way again, so remove their vote
+        setCurrentVote(undefined);
+        if (type === "UP") setVotesAmt((prev) => prev - 1);
+        else if (type === "DOWN") setVotesAmt((prev) => prev + 1);
+      } else {
+        // User is voting in the opposite direction, so subtract 2
+        setCurrentVote(type);
+        if (type === "UP") setVotesAmt((prev) => prev + (currentVote ? 2 : 1));
+        else if (type === "DOWN")
+          setVotesAmt((prev) => prev - (currentVote ? 2 : 1));
+      }
+    },
+  });
+
   return (
     <div className="flex gap-2 bg-zinc-100 rounded-full">
-      <Button size="sm" variant="ghost" aria-label="upvote">
+      <Button
+        size="sm"
+        variant="ghost"
+        aria-label="upvote"
+        onClick={() => vote("UP")}
+      >
         <ArrowBigUp
           className={cn(
             "h-5 w-5 text-zinc-700 hover:text-green-rad hover:fill-green-rad",
@@ -45,10 +98,15 @@ const PostVoteClient = ({
       </Button>
 
       <p className="text-center py-2 font-medium text-sm text-zinc-900 cursor-default">
-        {voteAmt}
+        {votesAmt}
       </p>
 
-      <Button size="sm" variant="ghost" aria-label="upvote">
+      <Button
+        size="sm"
+        variant="ghost"
+        aria-label="downvote"
+        onClick={() => vote("DOWN")}
+      >
         <ArrowBigDown
           className={cn(
             "h-5 w-5 text-zinc-700  hover:text-red-rad hover:fill-red-rad",
