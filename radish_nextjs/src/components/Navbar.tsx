bd05788b2ad724679bf1e8e2ebe4df26bc16d1c5
kaylee182000 <trungkien40414@gmail.com>
2023-07-02 22:20:16 +0700

    feat: Done creating search bar and functionality

--- radish_nextjs/src/components/Navbar.tsx
+++ radish_nextjs/src/components/Navbar.tsx
@@ -1,35 +1,30 @@
-import React from "react";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
-
-import { Icon, UserAccountNav } from "./";
 import { buttonVariants } from "./ui/Button";
-import { cn } from "@/lib/utils";
-import { getAuthSession } from "@/lib/auth";
+import { UserAccountNav, SearchBar, Icon } from "./";
 
 const Navbar = async () => {
-  const session = await getAuthSession();
-
+  const session = await getServerSession(authOptions);
   return (
-    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2">
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
-        {/* {logo} */}
-        <Link href={"/"} className="flex gap-2 items-center">
+        {/* logo */}
+        <Link href="/" className="flex gap-2 items-center">
           <Icon.logo />
-          <p className="hidden text-zinc-700 text-xl font-medium md:block">
-            Radishes
+          <p className="hidden text-zinc-700 text-sm font-medium md:block">
+            Breadit
           </p>
         </Link>
 
-        {/* {search bar} */}
+        {/* search bar */}
+        <SearchBar />
 
-        {/* {signin} */}
+        {/* actions */}
         {session?.user ? (
           <UserAccountNav user={session.user} />
         ) : (
-          <Link
-            href={"/sign-in"}
-            className={cn(buttonVariants(), "hover:bg-red-rad")}
-          >
+          <Link href="/sign-in" className={buttonVariants()}>
             Sign In
           </Link>
         )}
